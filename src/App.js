19d1ae989d59bfacf3ce9ffaf0beabd458641519
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-04-15 14:08:18 -0300

    Primeiro commit 15/04/23

--- src/App.js
+++ src/App.js
@@ -1,131 +1,14 @@
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
 import react from 'react';
 
 import {FaMicrosoft, FaQuestionCircle, FaAngleRight, FaExpandAlt, FaCloudShowersHeavy} from 'react-icons/fa'
 import { IoMdHome, IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { SiAdobecreativecloud } from 'react-icons/si';
-=======
-import {FaMicrosoft, FaQuestionCircle, FaExpandAlt, FaChevronRight, FaCloudShowersHeavy} from 'react-icons/fa'
->>>>>>> 0fc91907db76f0a84b2c591857f9c9fef681faf5
 
 
 function App(){ 
   return (
     <div>
-      <div className="cabecalho"> 
-      
-<<<<<<< HEAD
-          <FaMicrosoft>
-          </FaMicrosoft>
-=======
-      <FaMicrosoft>
-      </FaMicrosoft>
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
-
-            <a href="">           
-                previsão_tempo 
-            </a>
-
-            <FaAngleRight>
-            </FaAngleRight>
-
-            <span>web</span> 
-
-          <a href="">
-
-            <FaExpandAlt>
-            </FaExpandAlt>
-
-            <FaQuestionCircle>
-            </FaQuestionCircle>
-
-<<<<<<< HEAD
-          
-            Fazer logon
-          </a>
-=======
-      
-        Fazer logon
-      </a>
-=======
-        <a className='previsao'>           
-            previsão_tempo 
-            <FaChevronRight>
-            </FaChevronRight>
-           <span>web</span>
-        </a>
-
-        <FaExpandAlt>
-        </FaExpandAlt>
-
-        <FaQuestionCircle>
-        </FaQuestionCircle>
-
-        <a>
-          Fazer logon
-        </a>
-
-      </div>
->>>>>>> 0fc91907db76f0a84b2c591857f9c9fef681faf5
->>>>>>> refs/remotes/origin/main
-
-      </div>
-      
-      <div className="principla">
-<<<<<<< HEAD
-        
-=======
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/main
-        <div className='central'>
-
-          <div className='busca'>
-            <FaCloudShowersHeavy>
-            </FaCloudShowersHeavy>
-            <span>CLIMÃO.COM</span>
-          </div>
-
-        </div>
-<<<<<<< HEAD
-        
-=======
-=======
-        <div className='caixa'>
-          <div className="busca">
-            <FaCloudShowersHeavy>
-            </FaCloudShowersHeavy>
-            <h1>CLIMÃO.COM</h1>
-          </div>
-        </div>
-        
->>>>>>> 0fc91907db76f0a84b2c591857f9c9fef681faf5
->>>>>>> refs/remotes/origin/main
-      </div>
-
-      <div className="rodape">
-
-        <span className='adobe'>Criado com o Adobe XD</span>
-        <a href="">
-          <SiAdobecreativecloud>
-          </SiAdobecreativecloud>
-        
-          <IoMdHome>
-          </IoMdHome>
-          <IoIosArrowBack>
-          </IoIosArrowBack>
-        </a>
-        <span>1 de 2</span>
-        <a href="">
-          <IoIosArrowForward>
-          </IoIosArrowForward>
-        </a>
-      </div>
-
+      <h1>Projeto</h1>
     </div>
   );
 }
