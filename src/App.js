5c97a7395d82899c9dd6f31f13f0a1d89e3914db
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-04-15 23:43:42 -0300

    Segundo commit 15/04/23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import react from 'react';
-
+import { Link } from 'react-router-dom';
 import {FaMicrosoft, FaQuestionCircle, FaAngleRight, FaExpandAlt, FaCloudShowersHeavy} from 'react-icons/fa'
 import { IoMdHome, IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { SiAdobecreativecloud } from 'react-icons/si';
@@ -7,9 +7,35 @@ import { SiAdobecreativecloud } from 'react-icons/si';
 
 function App(){ 
   return (
-    <div>
-      <h1>Projeto</h1>
-    </div>
+      <div>
+        <div className='cabecalho1'>
+          <FaMicrosoft></FaMicrosoft>
+          <a href="">previsao_tempo</a>
+          <IoIosArrowForward></IoIosArrowForward>
+          <span>web</span>
+        </div>
+        <div className="cabecalho2"> 
+          <option>100%</option>
+          <a href="">
+            <FaExpandAlt></FaExpandAlt>
+            <FaQuestionCircle></FaQuestionCircle>
+            Fazerlogon</a>
+        </div>
+
+        <div className='principal'>
+            <div className='busca'>
+               <FaCloudShowersHeavy></FaCloudShowersHeavy>
+               <span>CLIMÃO.COM</span>
+               <input placeholder='Buscar cidade'></input>
+            </div>
+            <div className='central'>
+              central
+            </div>     
+            <div className='rodape'>
+              <span>Criado com o Adobe XD</span>
+            </div> 
+        </div>
+      </div>
   );
 }
 
